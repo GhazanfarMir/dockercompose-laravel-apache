fa5283447dc78df82e982f97c6e31d44267f9939
Ghazanfar Mir <GhazanfarMir@gmail.com>
2018-02-19 16:01:43 +0000

    Use .env file instead of adding Environment Variables in shell wrapper

--- env_up.sh
+++ env_up.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 
-# Setup Environment Variables
-export APP_PATH=${APP_PORT:-"${PWD}/app"}
-export APP_PORT=${APP_PORT:-9000}
-export DOCUMENT_ROOT=${DOCUMENT_ROOT:-"/var/www/html"}
-export DB_PORT=${DB_PORT:-13306}
-export DB_NAME=${DB_NAME:-dockerphp}
-export DB_ROOT_PASSWORD=${DB_ROOT_PASSWORD:-dockerphp}
-
 # Lets build the image
 docker-compose up -d --build
